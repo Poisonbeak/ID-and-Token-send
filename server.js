e3c6729a7cf3fad4f506d05a060150af8ccf58df
Poisonbeak <michele.alfano@davincimilazzo.edu.it>
2025-03-07 13:19:38 +0100

    POST response

--- server.js
+++ server.js
@@ -13,13 +13,33 @@ app.get("/", (req, res) => {
 // http://localhost:3333/users?id=1&token=abc
 app.get("/users", (req, res) => {
     console.log(req.query);
-    const userId = req.query.id;
-    const token = req.query.token;
+    const userId = req.query.id || -1;      // query nell'URL separati da &
+    const token = req.query.token || -1;
     // token validation
     const date = new Date();
     console.log(`User ${userId} logged in at ${date.toUTCString()}`);
     res.status(200).send("OK");
 });
+
+app.get("/users/:id/:token", (req, res) => {
+    console.log(req.params);
+    const userId = req.params.id;           // params nell'URL separati da /
+    const token = req.params.token;
+    // token validation
+    const date = new Date();
+    console.log(`User ${userId} logged in at ${date.toUTCString()}`);
+    res.status(200).send("OK");
+})
+
+app.post("/users", (req, res) => {
+    console.log(req.body);
+    const userId = req.body.id;      // body della richiesta formato con URLSearchParams
+    const token = req.body.token;
+    // token validation
+    const date = new Date();
+    console.log(`User ${userId} logged in at ${date.toUTCString()}`);
+    res.status(200).send("OK");
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
